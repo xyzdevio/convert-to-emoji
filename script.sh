de16c72b8b4b43ce9f95c38c90a29ef2b3221db2
xyzdevio <me@xyzdev.io>
2022-10-23 12:44:44 -0700

    update
    Signed-off-by: xyzdevio <me@xyzdev.io>

--- script.sh
+++ script.sh
@@ -10,11 +10,11 @@ list=$(grep -o "\\:\\S*\\:" $filename 2>/dev/null || true)
 # search for emoji corresponding img and warn but still continue if not found
 # replace all valid emoji elements and use github upload of img file (aka use the github hosted url version)
 for item in $list; do
-    len=${#item}-1
+    len=$(${#item}-1)
     str=${item:1:$len}
     echo $item
     echo ${#item}
-    echo ${#item}-1
+    echo $(${#item}-1)
     echo $len
     echo $str
 done
