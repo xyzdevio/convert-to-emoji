14bf5a11c900c92ecb43b22df2f29f074dfecc9e
xyzdevio <me@xyzdev.io>
2022-10-23 12:58:24 -0700

    update
    Signed-off-by: xyzdevio <me@xyzdev.io>

--- script.sh
+++ script.sh
@@ -10,12 +10,21 @@ list=$(grep -o "\\:\\S*\\:" $filename 2>/dev/null || true)
 # search for emoji corresponding img and warn but still continue if not found
 # replace all valid emoji elements and use github upload of img file (aka use the github hosted url version)
 for item in $list; do
+    # offset beg and end of str to remove pre/post-pend `:` elements
     len=$((${#item}-1))
     str=${item:1:$(($len-1))}
-    echo $item
-    echo ${#item}
-    echo $(($len-1))
-    echo $len
+    # working str
     echo $str
+    # find in github api
+    # if found
+    if true; do
+        echo "converting - $item"
+    else; do
+        echo "WARNING - $item is not a viable github emoji to convert"
+        continue
+    done
+    # replace str in file as img link
+    github_url="https://github.githubassets.com/images/icons/emoji/unicode/1f4af.png?v8"
+    sed -i -e "/$item/![$item]($github_url)/g" $filename
 done
 
