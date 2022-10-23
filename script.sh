e1767d6080812d73981363c1794f60bba8a19657
xyzdevio <me@xyzdev.io>
2022-10-23 11:47:56 -0700

    add helper script
    Signed-off-by: xyzdevio <me@xyzdev.io>

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+filename=$1
+echo $filename
+
+# find potential emojis in text
+grep -o '\:\S*\:' filename
+
+# search for emoji corresponding img and warn but still continue if not found
+
+# replace all valid emoji elements and use github upload of img file (aka use the github hosted url version)
