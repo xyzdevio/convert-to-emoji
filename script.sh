f4aebe0c4dc7d3bf8ad078fff61ed32ed6d6178a
xyzdevio <me@xyzdev.io>
2022-10-23 12:35:53 -0700

    update
    Signed-off-by: xyzdevio <me@xyzdev.io>

--- script.sh
+++ script.sh
@@ -5,8 +5,11 @@ echo $filename
 
 # find potential emojis in text
 # ignore error 123 for cases where no match is found
-grep -o "\\:\\S*\\:" $filename 2>/dev/null || true
+list=$(grep -o "\\:\\S*\\:" $filename 2>/dev/null || true)
 
 # search for emoji corresponding img and warn but still continue if not found
-
 # replace all valid emoji elements and use github upload of img file (aka use the github hosted url version)
+for item in list; do
+    echo $item
+done
+
