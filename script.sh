5e11b7334f6e83c69947e614bcf08d5844f92e92
xyzdevio <me@xyzdev.io>
2022-10-23 12:32:19 -0700

    update
    Signed-off-by: xyzdevio <me@xyzdev.io>

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ echo $filename
 
 # find potential emojis in text
 # ignore error 123 for cases where no match is found
-grep -o "\\:\\S*\\:" $filename 2> /dev/null
+grep -o "\\:\\S*\\:" $filename 2>/dev/null || true
 
 # search for emoji corresponding img and warn but still continue if not found
 
