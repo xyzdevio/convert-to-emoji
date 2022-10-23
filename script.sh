8dad8fb69c969eb4b6cb24f786ba961f4b449d9f
xyzdevio <me@xyzdev.io>
2022-10-23 16:30:51 -0700

    update
    Signed-off-by: xyzdevio <me@xyzdev.io>

--- script.sh
+++ script.sh
@@ -9,6 +9,7 @@ list=$(grep -o "\\:\\S*\\:" $filename 2>/dev/null || true)
 
 # search for emoji corresponding img and warn but still continue if not found
 # replace all valid emoji elements and use github upload of img file (aka use the github hosted url version)
+github_json=$(curl https://api.github.com/emojis)
 for item in $list; do
     # offset beg and end of str to remove pre/post-pend `:` elements
     len=$((${#item}-1))
@@ -16,18 +17,17 @@ for item in $list; do
     # working str
     echo $str
     # find in github api
-    github_url=""
-    unicode=""
-    ret=$(curl https://api.github.com/emojis)
-    echo "$ret" | jq -c  ".[\"$str\"]"
+    github_url=$(echo "$github_json" | jq -c  ".[\"$str\"]")
     # TODO - need a way to grab from 59->just the length until a non letter char is instroduced
     # and have render proper unicode char for that
 
-    if true
+    # if unicode val is found (aka not null), then handle
+    unicode=""
+    if [[ ! -z "$github_url" ]]
     then
         # TODO - instead of using github url for the full image, need to grab
         # the actual unicode character U+1f4af
-        github_url="https://github.githubassets.com/images/icons/emoji/unicode/1f4af.png?v8"
+        #github_url="https://github.githubassets.com/images/icons/emoji/unicode/1f4af.png?v8"
         unicode="\U${github_url:59:5}"
         unicode=${unicode^^}
         echo $unicode
